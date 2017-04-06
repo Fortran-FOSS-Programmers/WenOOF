fe172b6aa3fab0aa0c5093d0847b69df5afdd04c
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-04-06 17:16:51 +0200

    wrapped by ctypes, does not work well

--- src/lib/wenoof.F90
+++ src/lib/wenoof.F90
@@ -2,6 +2,7 @@
 module wenoof
 !< WenOOF, WENO interpolation Object Oriented Fortran library
 
+use, intrinsic :: iso_c_binding
 #ifdef r16p
 use penf, only: I_P, RPP=>R16P
 #else
@@ -33,4 +34,14 @@ contains
                       face_right=face_right,               &
                       eps=eps)
   endsubroutine wenoof_create
+
+  subroutine wenoof_reconstructor(S, stencil, interpolation) bind(c, name='wenoof_reconstructor')
+  integer(C_INT32_T), intent(in)          :: S                      !< Stencils number/dimension.
+  real(C_DOUBLE),     intent(in)          :: stencil(1:2, 1-S:-1+S) !< Stencil of the interpolation [1:2, 1-S:-1+S].
+  real(C_DOUBLE),     intent(out)         :: interpolation(1:2)     !< Result of the interpolation, [1:2].
+  class(interpolator_object), allocatable :: reconstructor          !< The WENO reconstructor.
+
+  call wenoof_create(interpolator_type='reconstructor-JS', S=S, interpolator=reconstructor)
+  call reconstructor%interpolate(stencil=stencil, interpolation=interpolation)
+  endsubroutine wenoof_reconstructor
 endmodule wenoof
